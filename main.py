eb4b5810c31d2e387d89d95eff74e1354721cca5
valerianoGonzalez <valerianogonzalez070402@gmail.com>
2024-06-20 16:49:46 -0400

    testing task management

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 
-
-#from task_management.task_manager import TaskManager: Imports TaskManager class from task_manager.py
-from task_management_copy import TaskManager
-
-#Enters a loop to display a menu and handle user input for various operations andd executes operations based on user input
+from task_management_copy_copy import TaskManager
 def main():
-    task_manager = TaskManager()
+    task_manager = TaskManager
 
     while True:
         print("\n===== Task Manager =====")
@@ -13,29 +9,26 @@ def main():
         print("2. Create a Task")
         print("3. Update a Task")
         print("4. Delete a Task")
-        print("5 filter tasks")
-        print("6. Exit")
+        print("5. Filter Tasks")
+        print("6. Classify Tasks")
+        print("7. Exit")
         print("====================")
 
-#Get user input for menu choice
         choice = input("Please select an option: ")
 
-#Process user choice
         if choice == "1":
-#Option 1: List all tasks
             task_manager.list_tasks()
         elif choice == "2":
-#Option 2: Create a new task
             description = input("Enter task description: ")
             deadline = input("Enter deadline: ")
-            priority = input("Enter priority: ")
+            priority = input("Enter priority (A = CRUCIAL  B = IMPORTANT   C = SIGNIFICANT): ")
             category = input("Enter category: ")
-            task_manager.create_task(description, deadline, priority, category)
+            status = input("enter status")
+            task_manager.create_task(description, deadline, priority, category,status)
         elif choice == "3":
-#Option 3: Update an existing task
             task_id = int(input("Enter task ID to update: "))
             description = input("Enter new description (press Enter to keep current): ")
-            deadline = input("Enter new deadline (press Enter to keep current): ")
+            deadline = input("Enter deadline (YYYY-MM-DD):  ")
             priority = input("Enter new priority (press Enter to keep current): ")
             category = input("Enter new category (press Enter to keep current): ")
             status = input("Enter new status (press Enter to keep current): ")
@@ -48,26 +41,21 @@ def main():
             }
             task_manager.update_task(task_id, **update_data)
         elif choice == "4":
-#Option 4: Delete an existing task
             task_id = int(input("Enter task ID to delete: "))
             task_manager.delete_task(task_id)
         elif choice == "5":
-#Option 5: Filter tasks by status
-            filter_type = input("Type the type of filter (Category, Priority or Deadline): ").lower()
+            filter_type = input("Type the type of filter (Category, Priority,status or Deadline): ").lower()
             filter_value = input(f"Type the value to filter by {filter_type}: ")
             task_manager.filter_tasks(filter_type, filter_value)
-        #Option 6: clasify
-        elif choice =="6":
-         classification_type = input("Enter the classification type: ")
-         task_manager.clasify_task(classification_type)
-          #Option 7: Exit the program
+        elif choice == "6":
+            classification_type = input("Enter the classification type: ")
+            task_manager.classify_task(classification_type)
         elif choice == "7":
             print("Exiting, please wait...")
-            break  
+            break
         else:
-#Handles invalid input
             print("Invalid choice. Please choose a number from the menu.")
-#After exiting the loop, save tasks to file
+
     print("Saving tasks...")
     task_manager.save_tasks()
     print("Tasks saved successfully.")
--- task_management_copy.py
+++ task_management_copy.py
@@ -5,221 +5,125 @@ from datetime import datetime
 
 tasklocation = "./task.json"
 
-def TaskManager():
-    
-# Global variables 
- 
+class TaskManager:
+    def __init__(self):
+        self.tasklocation = tasklocation
 
-# Function to create a new task
- def create_task(description, deadline, priority, category, status):
-    # Generate a unique ID for the task 
-    id_task = str(uuid.uuid4()) 
+    # Function to create a new task
+    def create_task(self, description, deadline, priority, category, status):
+        # Generate a unique ID for the task
+        id_task = str(uuid.uuid4())
 
-    # Create a dictionary representing the new task 
-    new_task = {
-        "id": id_task,
-        "description": description,
-        "deadline": deadline,
-        "priority": priority.upper(),  # Convert priority to uppercase 
-        "category": category,
-        "status": status.upper()[0],  # Convert status to uppercase and take the first character
-    }
+        # Create a dictionary representing the new task
+        new_task = {
+            "id": id_task,
+            "description": description,
+            "deadline": deadline,
+            "priority": priority.upper(),  # Convert priority to uppercase
+            "category": category,
+            "status": status.upper()[0],  # Convert status to uppercase and take the first character
+        }
 
-    # Read existing tasks from the JSON file
-    tasks = read_task()
+        # Read existing tasks from the JSON file
+        tasks = self.read_task()
 
-    # Add the new task to the list
-    tasks.append(new_task)
+        # Add the new task to the list
+        tasks.append(new_task)
 
-    # Write the updated list of tasks to the JSON file
-    write_task(tasks)
+        # Write the updated list of tasks to the JSON file
+        self.write_task(tasks)
 
-# Function to read tasks from the JSON file
-def read_task():
-    if os.path.exists(tasklocation):
-        with open(tasklocation, 'r') as data_file:
-            try:
-                tasks = json.load(data_file)
-            except json.JSONDecodeError:
-                tasks = []
-    else:
-        tasks = []
-
-    return tasks
-
-# Function to write tasks to the JSON file
-def write_task(tasks):
-    with open(tasklocation, 'w') as task_file:
-        json.dump(tasks, task_file, indent=4)
-
-# Function to list all tasks
-def list_tasks():
-    # Open the task file and display all tasks
-    tasks = read_task()
-    if not tasks:
-        print("No tasks found.")
-        return
-    
-    for i, task in enumerate(tasks, start=1):
-        description = task['description']
-        priority = task['priority']
-        deadline = task['deadline']
-        category = task['category']
-        status = task['status']
-        print(f"{'-'*10}#{i} {description} {'-'*10}\n  Category: {category} Priority: {priority}   Status: {status}   Deadline: {deadline}   ")
-
-
-# Example usage
-if __name__ == "__main__":
-    description = input("Enter task description: ")
-    
-    # Validate the deadline input
-    while True:
-        deadline = input("Enter deadline (YYYY-MM-DD): ")
-        try:
-            datetime.strptime(deadline, "%Y-%m-%d")
-            break
-        except ValueError:
-            print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
-    
-    # Validate priority
-    validationPriority = ['A', 'B', 'C']
-    while True:
-        priority = input("Enter priority (A = CRUCIAL  B = IMPORTANT   C = SIGNIFICANT): ").upper()
-        if priority in validationPriority:
-            break
+    # Function to read tasks from the JSON file
+    def read_task(self):
+        if os.path.exists(self.tasklocation):
+            with open(self.tasklocation, 'r') as data_file:
+                try:
+                    tasks = json.load(data_file)
+                except json.JSONDecodeError:
+                    tasks = []
         else:
-            print("Invalid format. You have to write A, B, or C.")
-    
-    category = input("Enter category: ").upper()
-    
-    # Validate status
-    validationStatus = ['P', 'I', 'C']
-    while True:
-        status = input("Enter status (P = PENDING  I = IN PROGRESS...  C = completed): ").upper()
-        if status in validationStatus:
-            break
-        else:
-            print("Invalid format. You have to write P, I, or C.")
+            tasks = []
 
-#filter 
+        return tasks
 
+    # Function to write tasks to the JSON file
+    def write_task(self, tasks):
+        with open(self.tasklocation, 'w') as task_file:
+            json.dump(tasks, task_file, indent=4)
 
+    # Function to list all tasks
+    def list_tasks(self):
+        # Open the task file and display all tasks
+        tasks = self.read_task()
+        if not tasks:
+            print("No tasks found.")
+            return
 
-valid_filters = ["priority", "status", "category", "deadline"]
+        for i, task in enumerate(tasks, start=1):
+            description = task['description']
+            priority = task['priority']
+            deadline = task['deadline']
+            category = task['category']
+            status = task['status']
+            print(f"{'-'*10}#{i} {description} {'-'*10}\n  Category: {category} Priority: {priority}   Status: {status}   Deadline: {deadline}   ")
 
-while True:
-    filter_type = input("Ingresa una clasificación (Priority, Status, Category, Deadline): ").lower()
-    if filter_type in valid_filters:
-        break
-    else:
-        print("Clasificación inválida. Inténtalo de nuevo.")
+    # Function to update a task
+    def update_task(self, task_id, **kwargs):
+        tasks = self.read_task()
+        for task in tasks:
+            if task['id'] == task_id:
+                task.update(kwargs)
+                break
+        self.write_task(tasks)
 
-filter_value = input(f"Type the value to filter by {filter_type}: ").upper()
+    # Function to delete a task
+    def delete_task(self, task_id):
+        tasks = self.read_task()
+        tasks = [task for task in tasks if task['id'] != task_id]
+        self.write_task(tasks)
 
-def filter_tasks(filter_type, filter_value):
-    task_number = 0
-    with open('dataBase.json', 'r') as file:
-        data = json.load(file)
-        print("-" * 10, filter_value, "-" * 10)
-        for task in data:
-            # Filter by category
-            if filter_type == 'category' and task['category'] == filter_value:
-                task_number += 1
-                print(f"#{task_number} Task: {task['description']}  Category: {filter_value}   Deadline: {task['deadline']}  Priority: {task['priority']} Status: {task['status']}")
-            # Filter by priority
-            elif filter_type == 'priority' and task['priority'] == filter_value:
-                task_number += 1
-                print(f"#{task_number} Task: {task['description']} Priority: {filter_value}  Deadline: {task['deadline']}  Category: {task['category']} Status: {task['status']}")
-            # Filter by deadline
-            elif filter_type == 'deadline' and task['deadline'] == filter_value:
-                task_number += 1
-                print(f"#{task_number} Task: {task['description']} Deadline: {filter_value} Priority: {task['priority']} Category: {task['category']} Status: {task['status']}")
-            # Filter by status
-            elif filter_type == 'status' and task['status'] == filter_value:
-                task_number += 1
-                print(f"#{task_number} Task: {task['description']} Status: {filter_value}  Deadline: {task['deadline']} Priority: {task['priority']} Category: {task['category']} Status: {filter_value}")
+    # Function to filter tasks
+    def filter_tasks(self, filter_type, filter_value):
+        tasks = self.read_task()
+        filtered_tasks = [task for task in tasks if task[filter_type] == filter_value.upper()]
 
-    if task_number == 0:
-        print(f"Lo siento, no tienes tareas con {filter_type} '{filter_value}'")
+        if not filtered_tasks:
+            print(f"No tasks found with {filter_type} '{filter_value}'")
+            return
+
+        for i, task in enumerate(filtered_tasks, start=1):
+            description = task['description']
+            priority = task['priority']
+            deadline = task['deadline']
+            category = task['category']
+            status = task['status']
+            print(f"{'-'*10}#{i} {description} {'-'*10}\n  Category: {category} Priority: {priority}   Status: {status}   Deadline: {deadline}   ")
+
+    # Function to classify tasks
+    def classify_tasks(self, classification_type):
+        tasks = self.read_task()
+
+        if classification_type == "priority":
+            priorities = ['A', 'B', 'C']
+            for priority in priorities:
+                print(f"{'-'*10} PRIORITY {priority} {'-'*10}")
+                for i, task in enumerate(tasks, start=1):
+                    if task['priority'] == priority:
+                        print(f"#{i} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Priority: {task['priority']} Status: {task['status']}")
         
-
-#clasification
-
-
-valid_classifications = ["priority", "status", "category","deadline"]
-
-while True:
-    classification_type = input("Ingresa una clasificación (Priority, Status, Category, Deadline): ").lower()
-    if classification_type in valid_classifications:
-        break
-    else:
-        print("Clasificación inválida. Inténtalo de nuevo.")
-
-with open('dataBase.json', 'r') as file:
-    data = json.load(file)
-
-def classify_task(classification_type):
-    # Clasificación por prioridad
-    if classification_type == "priority":
-        task_numbers = 0
-        print("-" * 10, "PRIORITY A", "-" * 10)
-        for task in data:
-            if task['priority'] == 'A':
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Priority: {task['priority']} Status: {task['status']}")
-
-        task_numbers = 0
-        print("-" * 10, "PRIORITY B", "-" * 10)
-        for task in data:
-            if task['priority'] == 'B':
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Priority: {task['priority']} Status: {task['status']}")
-
-        task_numbers = 0
-        print("-" * 10, "PRIORITY C", "-" * 10)
-        for task in data:
-            if task['priority'] == 'C':
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Priority: {task['priority']} Status: {task['status']}")
-
-    # Clasificación por estado
-    elif classification_type == "status":
-        task_numbers = 0
-        print("-" * 10, "PENDING", "-" * 10)
-        for task in data:
-            if task['status'] == 'P':
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Status: {task['status']}")
-
-        task_numbers = 0
-        print("-" * 10, "IN PROGRESS", "-" * 10)
-        for task in data:
-            if task['status'] == 'B':
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Status: {task['status']}")
-
-        task_numbers = 0
-        print("-" * 10, "COMPLETED", "-" * 10)
-        for task in data:
-            if task['status'] == 'C':
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Status: {task['status']}")
-
-    # Clasificación por categoría
-    elif classification_type == 'category':
-        UserCategory = input("Ingresa el nombre de la categoría: ")
-        print("-" * 10, UserCategory, "-" * 10)
-        task_numbers = 0
-        for task in data:
-            if task['category'] == UserCategory:
-                task_numbers += 1
-                print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Status: {task['status']}")
-                
-    elif classification_type == "deadline":
-        sorted_tasks = sorted(data, key=lambda task: datetime.strptime(task['deadline'], '%Y-%m-%d'))
-        task_numbers = 1
-        for task in sorted_tasks:
-            print(f"#{task_numbers} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Status: {task['status']}")
-            task_numbers += 1         
-
+        elif classification_type == "status":
+            statuses = ['P', 'I', 'C']
+            status_names = {'P': 'PENDING', 'I': 'IN PROGRESS', 'C': 'COMPLETED'}
+            for status in statuses:
+                print(f"{'-'*10} {status_names[status]} {'-'*10}")
+                for i, task in enumerate(tasks, start=1):
+                    if task['status'] == status:
+                        print(f"#{i} Task: {task['description']} Deadline: {task['deadline']} Category: {task['category']} Priority: {task['priority']} Status: {task['status']}")
+        
+        elif classification_type == 'category':
+            categories = set(task['category'] for task in tasks)
+            for category in categories:
+                print(f"{'-'*10} CATEGORY {category} {'-'*10}")
+                for i, task in enumerate(tasks, start=1):
+                    if task['category'] == category:
+                        print(f"#{i} Task:")
